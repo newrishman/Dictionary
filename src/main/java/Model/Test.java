176df9037f6accb5b7185077df253ff67e0e0ac1
newrishman <safarovrishat@gmail.com>
2019-07-17 01:20:54 +0300

    Simple Code

--- src/main/java/Model/Test.java
+++ src/main/java/Model/Test.java
@@ -2,9 +2,8 @@ package Model;
 
 public class Test {
     public static void main(String[] args) {
-        UserInput userInput = new UserInput();
         Dictionary dictionary = new Dictionary();
-        dictionary.dictionary(userInput.input());
+        dictionary.dictionary(new UserInput().input());
 
     }
 }
