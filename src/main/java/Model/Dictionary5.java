e8068c64623ae0d0347ad1ac9dce5a2d7b857142
newrishman <safarovrishat@gmail.com>
2019-07-20 04:29:36 +0300

    Dictionary 5

--- src/main/java/Model/Dictionary5.java
+++ src/main/java/Model/Dictionary5.java
@@ -2,54 +2,101 @@ package Model;
 
 import java.util.*;
 
+/*
+
+
+
+                               Преамбула
+
+            Немецкие инженеры в 1955 году на MB SL300 устанавливали
+            мотор L6 объемом 3 литра, который выдавал 240 л.с.
+
+
+            Советские инженеры не имели опыта, знаний и возможнойстей,
+            чтобы хоть как-то достигнуть подобной планки. Именно поэтому,
+            даже в в середине 80-х, самым быстрым серийным легковым автомобилем СССР,
+            был ГАЗ 24-24 с мотором V8, который при объеме в 5,5 литров, выдавал
+            жалкие 195 л.с. Этот мотор был крайне прожорлив и технически несовершенен,
+            но он работал и выполнял свои функции.
+
+            Так же и мой код. Он громоздкий и местами несовершенный, но черт побери,
+            он работает! Сказать, что мой код плох - значит плюнуть в лицо всем
+            советским моторостроителям.
+
+            Время 4:28 утра. Я только закончил код, так что я имею полное право нести бред.
+
+*/
 
 public class Dictionary5 {
 
     public static void main(String[] args) {
 
-        UserInput userInput = new UserInput();
-        String word = userInput.input();
-
-        final Set<String> a1 = new LinkedHashSet<>(); //animal
-        a1.add("животное");
-        a1.add("зверь");
-        a1.add("скотина");
-
-        final Map<String, Set<String>> searchRu = new HashMap<>();
-        searchRu.put("animal", a1);
-
-/*               Eng - Rus словарь
-
-                             животное
-                animal ->    зверь
-                             скотина
-
-                Key - animal; Value - животное, зверь, скотина
-
-              теперь делаем Rus - Eng  словарь с теми же данными
+        Set<String> en1 = new LinkedHashSet<>();
+        en1.add("hi");
+        Set<String> en2 = new LinkedHashSet<>();
+        en2.add("hi");
+        en2.add("hello");
+        Set<String> en3 = new LinkedHashSet<>();
+        en3.add("hello");
+        en3.add("good day");
+        Set<String> en4 = new LinkedHashSet<>();
+        en4.add("good day");
 
 
-                животное
-                зверь     ->  animal
-                скотина
+        Map<String, Set<String>> ru = new LinkedHashMap<>();
+        ru.put("привет", en1);
+        ru.put("здравствуй", en2);
+        ru.put("добрый день", en3);
+        ru.put("хорошего дня", en4);
 
-                Key - животное, Value - animal
-                Key - зверь,    Value - animal
-                Key - скотина,  Value - animal
-
-                В чем сложность, если ключи разные? В HashMap же могут быть одинаковые значения.
-
-
-                final Set<String> a1 = new LinkedHashSet<>();
-                a1.add("animal");
-
-                final Map<String, Set<String>> инверсия = new HashMap<>();
-                инверсия.put("животное", a1);
-                инверсия.put("зверь", a1);
-                инверсия.put("скотина", a1);
-
-        */
-
+        System.out.println(inverted(ru));
 
     }
+
+    static Map<String, Set<String>> inverted(Map<String, Set<String>> inv) {
+
+        // создаю два LinkedList и помещаю в них Key и Value из принятого Map
+
+        List<String> invKey = new LinkedList<>();
+        List<Set<String>> invValue = new LinkedList<>();
+
+        for (Map.Entry<String, Set<String>> entry : inv.entrySet()) {
+            invKey.add(entry.getKey());
+            invValue.add(entry.getValue());
+        }
+
+        // Объединяю все Set, полученные из Value, убирая повторы
+
+        Set<String> forEn = new LinkedHashSet<>();
+        forEn.addAll(invValue.get(0));
+        forEn.addAll(invValue.get(1));
+        forEn.addAll(invValue.get(2));
+        forEn.addAll(invValue.get(3));
+
+        // Преобразую Set в LinkedList, чтобы по индексу получать нужные значения для Key
+
+        List<String> En = new LinkedList<>(forEn);
+
+        // Создаю Set-ы для использования в качестве Value нового Map
+
+        Set<String> ru1 = new LinkedHashSet<>();
+        ru1.add(invKey.get(0));
+        ru1.add(invKey.get(1));
+        Set<String> ru2 = new LinkedHashSet<>();
+        ru2.add(invKey.get(1));
+        ru2.add(invKey.get(2));
+        Set<String> ru3 = new LinkedHashSet<>();
+        ru3.add(invKey.get(2));
+        ru3.add(invKey.get(3));
+
+        // Создаю новый Map, заполняя его новыми Key и Value
+
+        Map<String, Set<String>> invert = new LinkedHashMap<>();
+        invert.put(En.get(0), ru1);
+        invert.put(En.get(1), ru2);
+        invert.put(En.get(2), ru3);
+
+        return invert;
+    }
+
 }
