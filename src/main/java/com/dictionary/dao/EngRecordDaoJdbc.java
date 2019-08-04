bf57f0a88015782d1f7e352faffa3163ab30e93d
newrishman <safarovrishat@gmail.com>
2019-08-05 02:14:48 +0300

    Super Dictionary

--- src/main/java/com/dictionary/dao/EngRecordDaoJdbc.java
+++ src/main/java/com/dictionary/dao/EngRecordDaoJdbc.java
@@ -17,15 +17,11 @@ public class EngRecordDaoJdbc implements EngRecordDao {
         connection = service.getConnection();
 
         word = inputs[2];
-        // команда 5 - поиск английского id
+        // команда #5 - поиск английского id
         engId = service.searchId(5, word);
-
-        System.out.println(engId + " - это id Eng если уже есть");
-
         if (engId == null) {
-            // команда 7 - запись английского слова
+            // команда #7 - запись английского слова
             engId = service.recordWord(7, word);
-            System.out.println(engId + " - это id Eng после записи");
         }
 
         for (int x = 3; x < inputs.length; x++) {
@@ -34,21 +30,14 @@ public class EngRecordDaoJdbc implements EngRecordDao {
             connection = service.getConnection();
 
             word = inputs[x];
-            System.out.println(word + " - записываемый перевод");
-            // команда 6 - поиск русского id
+            // команда #6 - поиск русского id
             rusId = service.searchId(6, word);
-
-            System.out.println(rusId + " - это id Rus если уже есть");
-
             if (rusId == null) {
-                // команда 8 - запись русского слова
+                // команда #8 - запись русского слова
                 rusId = service.recordWord(8, word);
 
-                System.out.println(rusId + " - это id Rus после записи");
             }
-            // команда 9 - запись русского и английского ID
-            System.out.println("Я хочу записать: " +engId +" и " + rusId);
-            System.out.println("  ");
+            // команда #9 - запись русского и английского ID
             service.recordId(9, engId, rusId);
 
         }
--- src/main/java/com/dictionary/dao/RusRecordDaoJdbc.java
+++ src/main/java/com/dictionary/dao/RusRecordDaoJdbc.java
@@ -5,6 +5,9 @@ import com.dictionary.service.Service;
 import java.sql.Connection;
 
 public class RusRecordDaoJdbc implements RusRecordDao {
+    private String word;
+    private Long engId;
+    private Long rusId;
     private Connection connection;
     private Service service;
 
@@ -13,5 +16,29 @@ public class RusRecordDaoJdbc implements RusRecordDao {
         service = new Service();
         connection = service.getConnection();
 
+        word = inputs[2];
+        // команда 6 - поиск русского id
+        rusId = service.searchId(6, word);
+        if (rusId == null) {
+            // команда 8 - запись русского слова
+            rusId = service.recordWord(8, word);
+        }
+
+        for (int x = 3; x < inputs.length; x++) {
+            service = new Service();
+            connection = service.getConnection();
+
+            word = inputs[x];
+            // команда 5 - поиск английского id
+            engId = service.searchId(5, word);
+            if (engId == null) {
+                // команда 7 - запись английского слова
+                engId = service.recordWord(7, word);
+
+            }
+            // команда 9 - запись русского и английского ID
+            service.recordId(9, engId, rusId);
+
+        }
     }
 }
